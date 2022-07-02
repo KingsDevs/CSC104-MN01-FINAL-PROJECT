12c3a278509c5c28628138550219a87ca36ee8d1
KingsDevs <sunnyckair@gmail.com>
2022-07-02 13:21:18 +0800

    added shade plugin

---
+++ src/main/java/com/csc104oop/shelves/Main.java
@@ -0,0 +1,11 @@
+package com.csc104oop.shelves;
+
+import java.io.IOException;
+
+public class Main 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        App.main(args);
+    }
+}
